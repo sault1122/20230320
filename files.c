73d657dfe9514fd7283f26436f4b213bf54c6946
sault1122 <1239442310@qq.com>
2023-12-01 18:46:40 +0800

    首次代码提交

---
+++ files.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+	cout<<"ccccc"<<endl;
+	cout<<"ccccc"<<endl;
+        cout<<"ccccc"<<endl;
+        cout<<"ccccc"<<endl;
+        cout<<"ccccc"<<endl;
+        cout<<"ccccc"<<endl;
+ 
+	return 0;
+}
